8eaf399441e26a027eee07f620171e83fd4ee3a6
Salih <101745582+Samed763@users.noreply.github.com>
2024-10-25 16:56:21 +0300

    Add files via upload

---
+++ libFT_1/ft_memcmp.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	char	*str1;
+	char	*str2;
+
+	str1 = s1;
+	str2 = s2;
+	i = 0;
+	while (i < n && ((const char)s1[i] != '\0' || (const char)s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+}
