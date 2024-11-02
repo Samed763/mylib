9f7dd32306e2241c90249b44915899e042e7cce0
Salih <101745582+Samed763@users.noreply.github.com>
2024-11-02 14:50:22 +0300

    Add files via upload

---
+++ libFT_1/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadinc <sdinc763@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 16:22:47 by sadinc            #+#    #+#             */
+/*   Updated: 2024/11/01 16:22:48 by sadinc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ret;
+
+	ret = malloc(count * size);
+	if (ret)
+		ft_bzero(ret, count * size);
+	return (ret);
+}
---
+++ libFT_1/ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadinc <sdinc763@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 16:47:29 by sadinc            #+#    #+#             */
+/*   Updated: 2024/11/01 16:48:13 by sadinc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*mem;
+
+	i = 0;
+	j = 0;
+	mem = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!mem)
+		return (NULL);
+	while (s1[i])
+	{
+		mem[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		mem[i + j] = s2[j];
+		j++;
+	}
+	mem[i + j] = '\0';
+	return (mem);
+}
