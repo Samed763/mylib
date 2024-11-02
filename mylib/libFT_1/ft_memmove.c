6190d01cd99364641d86dadbdac32715b2dc979f
Salih <101745582+Samed763@users.noreply.github.com>
2024-11-02 14:49:33 +0300

    Add files via upload

---
+++ mylib/libFT_1/ft_memmove.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadinc <sdinc763@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 16:48:43 by sadinc            #+#    #+#             */
+/*   Updated: 2024/11/02 12:50:42 by sadinc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+
+	i = len;
+	if (dst == src)
+		return (dst);
+	else if ((char *)dst > (char *)src)
+	{
+		while (i > 0)
+		{
+			i--;
+			((char *)dst)[i] = ((char *)src)[i];
+		}
+	}
+	else
+	{
+		ft_memcpy(dst, src, len);
+	}
+	return (dst);
+}
---
+++ mylib/libFT_1/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadinc <sdinc763@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 16:24:09 by sadinc            #+#    #+#             */
+/*   Updated: 2024/11/02 14:40:03 by sadinc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dst_len;
+	size_t	src_len;
+
+	i = 0;
+	j = 0;
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize == 0 || dst_len >= dstsize)
+		return (dstsize + src_len);
+	while (dst[i] && i < dstsize - 1)
+		i++;
+	while (j < src_len && (i + j) < (dstsize - 1))
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	dst[i + j] = '\0';
+	return (dst_len + src_len);
+}
